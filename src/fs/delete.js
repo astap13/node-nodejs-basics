8d927496e47198e0ffc2f39680b95f22825aea37
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-05 04:32:22 +0200

    feat: delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { unlink } from "node:fs/promises";
+import { access } from "node:fs/promises";
+import { join } from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join("src", "fs", "files", "fileToRemove.txt");
+  try {
+    await access(filePath);
+    await unlink(filePath, (error) => {
+      if (error) throw new Error("FS operation failed");
+    });
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await remove();
