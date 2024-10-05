24d254ed7f66b62bdc7f09d6f02c784239d5cb92
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-05 02:57:11 +0200

    feat: copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { cp } from "node:fs";
+import { access, mkdir } from "node:fs/promises";
+import { join } from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolderPath = join("src", "fs", "files");
+  const destFolderPath = join("src", "fs", "files_copy");
+
+  try {
+    await access(destFolderPath);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await mkdir(destFolderPath, { recursive: true });
+    } else {
+      throw err;
+    }
+  }
+
+  cp(srcFolderPath, destFolderPath, { recursive: true }, (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    } else {
+      console.log('Directory copied successfully');
+    }
+  });
 };
 
 await copy();
