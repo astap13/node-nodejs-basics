f81d97dc5c5792bb30a4dd2b32eb10b7c27e140b
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-05 04:27:02 +0200

    feat: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir } from "node:fs/promises";
+import { access } from "node:fs/promises";
+import { join } from "path";
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = join("src", "fs", "files");
+  
+  try {
+    await access(dirPath);
+    
+    const files = await readdir(dirPath);
+    console.log(files);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.error("Directory not found"); 
+    } else {
+      console.error("FS operation failed:", error.message);
+    }
+  }
 };
 
 await list();
