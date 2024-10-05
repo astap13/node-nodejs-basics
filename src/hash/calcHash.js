19da27886a821f21836ebeeee9dd6bb3a693e501
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-06 00:29:12 +0200

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createReadStream } from "node:fs";
+import { join } from "path";
+import { createHash } from "node:crypto";
+
+const calculateHash = () => {
+  const filePath = join("src", "hash", "files", "fileToCalculateHashFor.txt");
+
+  const hash = createHash('sha256');
+
+  const fileStream = createReadStream(filePath);
+
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const fileHash = hash.digest('hex');
+    console.log(fileHash);
+  });
+
+  fileStream.on('error', (err) => {
+    console.error("Error reading file:", err.message);
+  });
 };
 
-await calculateHash();
+calculateHash();
