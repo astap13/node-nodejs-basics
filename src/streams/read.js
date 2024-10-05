98f45db516e801d4cfadec1518dadbf48a0df965
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-06 01:35:59 +0200

    feat: read.js in stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'node:fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join('src', 'streams', 'files', 'fileToRead.txt');
+
+    const fileStream = createReadStream(filePath, { encoding: 'utf-8' });
+  
+    fileStream.on("data", (chunk) => {
+        process.stdout.write(chunk);
+      });
+    
+      fileStream.on("error", () => {
+        throw new Error("Stream operation failed");
+      });
 };
 
 await read();
