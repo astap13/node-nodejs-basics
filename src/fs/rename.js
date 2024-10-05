56d4a12605e1b80ca10cfd86106432ffa37486d9
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-05 03:13:22 +0200

    feat: rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { rename as fsRename } from "node:fs";
+import { access } from "node:fs/promises";
+import { join } from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFilePath = join("src", "fs", "files", "wrongFilename.txt");
+  const properFilePath = join("src", "fs", "files", "properFilename.md");
+  try {
+    await access(properFilePath);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      throw error;
+    }
+    fsRename(wrongFilePath, properFilePath, (err) => {
+      if (err) {
+        throw new Error("FS operation failed");
+      }
+    });
+  }
 };
 
 await rename();
