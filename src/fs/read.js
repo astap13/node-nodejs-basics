b2648d3935a80fe5a542499a365b8ae8005cbc8f
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-05 04:20:08 +0200

    feat: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
-};
+import { readFile } from "node:fs/promises";
+import { access } from "node:fs/promises";
+import { join } from "path";
 
+const read = async () => {
+  const filePath = join("src", "fs", "files", "fileToRead.txt");
+  try {
+    await access(filePath);
+    let fileContent = await readFile(filePath, { encoding: "utf-8" });
+    console.log(fileContent);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
+};
 await read();
