54340cc32eb8b921c4ae19e2ffd6e04f227148d1
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-07 14:27:33 +0200

    feat: cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from "child_process";
+import { join } from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = join("src", "cp", "files", "script.js");
+
+  const child = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
+
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);
