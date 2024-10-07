2941a87d9e85cfe91e20755b2dbec7382b4ff724
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-07 10:08:48 +0200

    feat: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from "node:fs";
+import { join } from "path";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join("src", "streams", "files", "fileToWrite.txt");
+
+  const fileStream = createWriteStream(filePath, { encoding: "utf-8" });
+
+  fileStream.on("error", (err) => {
+    throw new Error("Stream operation failed: " + err.message);
+  });
+
+  process.stdin.pipe(fileStream);
 };
 
 await write();
