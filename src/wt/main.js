bb6cab9f5f6800e05e33f855e2361ef1fa0e9ffa
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-07 14:09:16 +0200

    feat: mail.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { join, dirname } from "path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, "worker.js");
+
+const createWorkerPromise = (workerData) => {
+  return new Promise((resolve) => {
+    const worker = new Worker(filePath, { workerData });
+
+    worker.on("message", (message) => {
+      resolve({ status: "resolved", data: message });
+    });
+
+    worker.on("error", () => {
+      resolve({ status: "error", data: null });
+    });
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const numThreads = cpus().length;
+  const workerPromises = Array.from({ length: numThreads }, (_, i) => createWorkerPromise(i + 10));
+
+  const settledPromises = await Promise.allSettled(workerPromises);
+  const results = settledPromises.map(({ value }) => value);
+
+  console.log(results);
 };
 
 await performCalculations();
