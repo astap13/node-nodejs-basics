9101ed5f82a21fbb40439af7c87ae57d6e7cca76
Kiryl Astapenka <astapenko.1304@gmail.com>
2024-10-07 11:49:45 +0200

    feat: compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import {join} from 'path'
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = join('src', 'zip', 'files', 'fileToCompress.txt');
+    const zipFilePath = join('src', 'zip', 'files', 'archive.gz')
+
+    const gzip = createGzip();
+    const source = createReadStream(filePath);
+    const destination = createWriteStream(zipFilePath);
+
+pipeline(source, gzip, destination, (err) => {
+    if (err) {
+        console.error(err);
+        process.exitCode = 1;
+    }
+});
 };
 
 await compress();
